7f139740b75fa306159870da2fc3196336cd173c
Olivier LeDiouris <olivier.lediouris@gmail.com>
2016-04-27 17:30:10 -0700

    project builder

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+update() {
+  echo Updating $1...
+  cd olivsoft/$1
+  git pull --quiet origin master
+  cd ../..
+}
+#
+update javanmeaparser
+update coreutilities
+update chartcomponents
+update javanauticalalmanac
+update fullnmeaconsole
+update java-tide-engine
+update tide-engine-implementation
+update almanac-related-tools
+update google-locator
+update star-finder
+update sailfax
+update chart-library
+update navigation-desktop
+update polar-smoother
+update navigation-desktop-user-exits
+update geomutils
+update weatherwizard
+update release
+#
+echo Code update completed
+echo You can now run the build:
+echo 'Prompt> cd olivsoft/release'
+echo 'Prompt> ../../gradlew shadowJar'
+echo After that, you can run the soft just like before.
+#
